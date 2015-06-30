b0959d874e23699edbaf354e441f7e1675f7e100
Bondan23 <bondan.ekoprasetyo@gmail.com>
2015-07-01 05:13:25 +0700

    Revert "Menghapus Function Valid Email"
    This reverts commit ea3603f4297bd5f1b0816831caf3189550ecc590.

--- test.php
+++ test.php
@@ -2,6 +2,20 @@
 
 //jika di tambahin @ maka error tidak akan di tampilkan
 
+
+function validemail($mail)
+{
+	define('valid','bondan@bondan.com');
+	
+	if($mail !== valid)
+	{
+		return FALSE;
+	}
+	else 
+	{
+		return TRUE;
+	}
+}
 /*
 if(@$_POST['email'])
 {
